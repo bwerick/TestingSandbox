bf4031519ce0256352d1bde5002739ba8c114542
Erick Duarte <eduarte1@asu.edu>
2026-01-02 20:50:31 -0700

    Update dobot script and remove old lab2 file

--- dobot.py
+++ dobot.py
@@ -2,12 +2,16 @@ from pydobot import Dobot
 
 from serial.tools import list_ports
 import pydobot
+
 available_ports = list_ports.comports()
-print(f'available ports: {[x.device for x in available_ports]}')
+print(f"available ports: {[x.device for x in available_ports]}")
+
 port = available_ports[0].device
-device = pydobot.Dobot(port='/dev/tty.usbmodem2101', verbose=True)
+device = pydobot.Dobot(port="/dev/tty.usbmodem21301", verbose=True)
+device.move_to(x=240, y=0, z=150, r=0, wait=False)
 (x, y, z, r, j1, j2, j3, j4) = device.pose()
-print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
-device.move_to(x + 30, y, z, r, wait=False)
-device.move_to(x, y, z, r, wait=True)  # we wait until this movement is done before continuing
-device.close()
+print(f"x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}")
+# device.suck(False)
+
+# device.move_to(x, y, z, r, wait=True)  # we wait until this movement is done before continuing
+# device.close()
